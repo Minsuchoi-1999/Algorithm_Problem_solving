3d6279164a1a235b79a68f8a8f65f0c468b8b5c5
Minsu_Choi <102347404+Minsuchoi-1999@users.noreply.github.com>
2023-11-17 17:11:25 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 384 ms, Memory: 26728 KB -BaekjoonHub

---
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <stack>
+#include <climits>
+
+using namespace std;
+
+#define FOR(i, n) for (long long i = 0; i < n; i++)
+#define INF 1234567890
+#define MAX 1234567891
+#define MOD 1234567891
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N - 1)) // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((long long)pow(2.0, (long long)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+
+int n, s;
+vi arr;
+vector<long long> arr1, arr2;
+
+void dfs(int index, int last, long long sum, vector<long long> &psum)
+{
+    if (index == last)
+    {
+        psum.push_back(sum);
+        return;
+    }
+    dfs(index + 1, last, sum + arr[index], psum);
+    dfs(index + 1, last, sum, psum);
+}
+
+signed main(int argc, char **argv)
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        int key;
+        cin >> key;
+        arr.push_back(key);
+    }
+
+    dfs(0, n / 2, 0, arr1);
+    dfs(n / 2, n, 0, arr2);
+
+    sort(arr2.begin(), arr2.end());
+
+    long long cnt = 0;
+
+    for (auto e : arr1)
+    {
+        cnt += upper_bound(arr2.begin(), arr2.end(), s - e) - lower_bound(arr2.begin(), arr2.end(), s - e);
+    }
+
+    if (s == 0)
+    {
+        cnt -= 1;
+    }
+
+    cout << cnt;
+}
