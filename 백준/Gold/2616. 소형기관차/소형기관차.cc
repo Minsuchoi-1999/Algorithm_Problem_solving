4fc37249a23b270a4b1d0318c3915d5209086b09
Minsu_Choi <102347404+Minsuchoi-1999@users.noreply.github.com>
2023-11-27 11:26:24 +0900

    [Gold III] Title: 소형기관차, Time: 4 ms, Memory: 3780 KB -BaekjoonHub

---
+++ 백준/Gold/2616. 소형기관차/소형기관차.cc
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <stack>
+#include <climits>
+
+using namespace std;
+
+#define FOR(i, n) for (long long i = 0; i < n; i++)
+#define INF 1234567890
+#define MAX 1234567891
+#define MOD 1234567891
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N - 1)) // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((long long)pow(2.0, (long long)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+typedef long long ll;
+typedef pair<long long, long long> ii;
+typedef vector<long long> vi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+
+int n, m;
+
+int psum[50001] = {
+    0,
+};
+
+long long dp[4][50001] = {
+    0,
+};
+
+long long get_sum(int num)
+{
+    long long ret = psum[num] - psum[num - m];
+
+    return ret;
+}
+
+signed main(int argc, char **argv)
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int num;
+        cin >> num;
+        psum[i] = psum[i - 1] + num;
+    }
+
+    cin >> m;
+
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = i * m; j <= n; j++)
+        {
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j - m] + get_sum(j));
+        }
+    }
+
+    cout << dp[3][n];
+}
