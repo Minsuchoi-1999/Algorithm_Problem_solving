5569e5d19cc99a903b3f76985d4d415dce8a8707
Minsu_Choi <102347404+Minsuchoi-1999@users.noreply.github.com>
2023-11-27 14:22:05 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <stack>
+#include <climits>
+
+using namespace std;
+
+#define FOR(i, n) for (long long i = 0; i < n; i++)
+#define INF 1234567890
+#define MAX 1234567891
+#define MOD 1234567891
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N - 1)) // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((long long)pow(2.0, (long long)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+typedef long long ll;
+typedef pair<long long, long long> ii;
+typedef vector<long long> vi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+
+int n;
+
+int win[1001];
+
+signed main(int argc, char **argv)
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    win[1] = true;
+    win[2] = false;
+    win[3] = true;
+    win[4] = true;
+    for (int i = 5; i <= n; i++)
+        if (!win[i - 1] || !win[i - 3] || !win[i - 4])
+            win[i] = true;
+
+    if (win[n])
+        cout << "SK";
+    else
+        cout << "CY";
+}
