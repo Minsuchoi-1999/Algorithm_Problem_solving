06134319bd00e7669cf0eec05dcb63db41e66797
Minsu_Choi <102347404+Minsuchoi-1999@users.noreply.github.com>
2024-03-04 19:10:11 +0900

    [Gold III] Title: 아침 산책, Time: 316 ms, Memory: 15784 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.cc
+++ 백준/Gold/21606. 아침 산책/아침 산책.cc
@@ -5,27 +5,28 @@
 
 using namespace std;
 
-int n;
-int node_info[200001];
+long long n;
+long long node_info[200001];
 
-vector<vector<int>> graphs;
+vector<vector<long long>> graphs;
 
-int brute_force(int start, int cur)
+long long brute_force(long long prev, long long cur)
 {
-    if (start != cur && node_info[cur])
+    // cout << prev << " " << cur << "\n";
+    if (prev != cur && node_info[cur])
     {
         return 1;
     }
 
-    int ret = 0;
+    long long ret = 0;
 
-    for (int i = 0; i < graphs[cur].size(); i++)
+    for (long long i = 0; i < graphs[cur].size(); i++)
     {
-        int next = graphs[cur][i];
+        long long next = graphs[cur][i];
 
-        if (next != start)
+        if (next != prev)
         {
-            ret += brute_force(start, next);
+            ret += brute_force(cur, next);
         }
     }
 
@@ -39,25 +40,25 @@ int main()
     string infos;
     cin >> infos;
 
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
         node_info[i] = infos[i - 1] - '0';
     }
 
-    graphs = vector<vector<int>>(n + 1);
+    graphs = vector<vector<long long>>(n + 1);
 
-    for (int i = 0; i < n - 1; i++)
+    for (long long i = 0; i < n - 1; i++)
     {
-        int node1, node2;
+        long long node1, node2;
         cin >> node1 >> node2;
 
         graphs[node1].push_back(node2);
         graphs[node2].push_back(node1);
     }
 
-    int roads = 0;
+    long long roads = 0;
 
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
         if (node_info[i])
         {
